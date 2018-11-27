e57dc987e17cc1814b173fc12f65a4f1bf187cfa
ipek <ipekcivicakan@gmail.com>
2018-11-27 15:31:27 -0800

    Configuring connect with MapStateToProps

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import SongList from "./SongList";
 
 const app = () => {
-  return <div> App</div>;
+  return (
+    <div>
+      <SongList />
+    </div>
+  );
 };
 
 export default app;
